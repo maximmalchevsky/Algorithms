1139f2e03b40bbd455867691790825f6319dc0c2
Maxim Malchevsky <144607112+maximmalchevsky@users.noreply.github.com>
2024-03-08 11:47:34 +0300

    .

---
+++ Sorts/bubbleSort.ts
@@ -0,0 +1,13 @@
+function bubbleSort(array: number[]): number[] {
+        for (let i = 0; i < array.length; i++){
+            for (let j = 0; j < array.length; j++){
+                if (array[j] > array[j + 1]){
+                    let temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        return array
+}
